bcc72a13a0b88fa5e78920cbe9e46ccb62d84e02
toplap barcelona <rogerpibernat@gmail.com>
2023-12-17 10:17:27 +0100

    Add OSC

--- main.gd
+++ main.gd
@@ -1,29 +1,45 @@
 extends Node2D
 
+var osc: OscReceiver
+var soundServerOscIp := "127.0.0.1"
+var soundServerOscPort := 4001
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	Log.setLevel(Log.LOG_LEVEL_VERBOSE)
+	osc = OscReceiver.new()
+	self.add_child.call_deferred(osc)
+	osc.startServer()
+	#OS.execute("zynaddsubfx", ["-P", soundServerOscPort])
+	$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventKey and event.pressed:
+	if not event is InputEventKey: return
+	if event.pressed:
 		match event.keycode:
+			KEY_SPACE:
+				sendOsc("/noteOn", [0, 60, randi() % 96 + 12])
 			KEY_TAB:
 				focusNext()
 				_ignoreEvent()
+	if event.is_released():
+		match event.keycode:
+			KEY_SPACE:
+				sendOsc("/noteOff", [0, 60])
 
-func sendOsc(msg: String):
-	print("Send OSC: %s" % [msg])
+func sendOsc(addr: String, args: Array):
+	#Log.verbose("%s %s" % [addr, args])
+	osc.sendMessage("%s/%s" % [soundServerOscIp, soundServerOscPort], addr, args)
 
 func focusNext():
 	var viewport = get_parent().get_viewport()
 	var focused = viewport.gui_get_focus_owner()
 	var parent = focused.get_parent()
-	var index = focused.get_index() + 1 % parent.get_child_count()
+	var index = focused.get_index() + 1 % (parent.get_child_count())
 	var next = parent.get_child(index)
 	next.grab_focus()
 	print("TODO main.focusNext")
@@ -31,3 +47,8 @@ func focusNext():
 func _ignoreEvent():
 	# disable the event at the Window level, the hierarchy is (Window.Main.TextEdit.self)
 	get_parent().set_input_as_handled()
+
+
+func _on_timer_timeout():
+	#sendOsc("/noteOn", [0, randi() % 127 , 60])
+	pass
