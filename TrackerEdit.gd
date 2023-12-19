2daf17510e2afe70fc00e4e5550fb0ff5834ca69
toplap barcelona <rogerpibernat@gmail.com>
2023-12-19 12:58:37 +0100

    Fix input

--- TrackerEdit.gd
+++ TrackerEdit.gd
@@ -27,36 +27,58 @@ func _input(event):
 	if event is InputEventKey and event.pressed:
 		match event.keycode:
 			KEY_TAB: 
-				moveRight()
+				nextColumn()
 				_ignoreEvent()
-			KEY_RIGHT: moveRight()
-			KEY_LEFT: moveLeft()
+			KEY_RIGHT: nextColumn()
+			KEY_LEFT: prevColumn()
+			KEY_SPACE: 
+				nextColumn()
+				_ignoreEvent()
 			KEY_ENTER: 
 				select_word_under_caret()
 				var sel = get_selected_text()
 				Log.warn("<ENTER> key is disabled in `TrackerEdit.gd`")
 				_ignoreEvent()
 			_:
-				insertText(event.as_text())
-				_ignoreEvent()
+				if event.keycode >= KEY_A and event.keycode <= KEY_Z:
+					insertText(event.as_text())
+					_ignoreEvent()
 
 func _ignoreEvent():
 	# disable the event at the Window level, the hierarchy is (Window.Main.TextEdit.self)
 	get_parent().get_parent().get_parent().set_input_as_handled()
 
 func insertText(str: String):
-	var x = get_text()
+	var lnText = get_line(get_caret_line()).substr(get_caret_column())
+	if isEOC(): nextColumn()
+	if isEOL(): set_caret_column(get_caret_column() - 1)
 	remove_text(get_caret_line(), get_caret_column(), get_caret_line(), get_caret_column() + 1)
 	insert_text_at_caret(str)
 
-func moveRight():
+## Is end of column
+func isEOL() -> bool:
 	var ln := get_caret_line()
 	var lineText := get_line(ln)
-	var pos := lineText.find(" ", get_caret_column())
+	var next = get_line(get_caret_line()).substr(get_caret_column(),1)
+	return next.unicode_at(0) == 0
+
+## Is end of column
+func isEOC() -> bool:
+	var ln := get_caret_line()
+	var lineText := get_line(ln)
+	var next = get_line(get_caret_line()).substr(get_caret_column(),1)
+	return next == " "
+
+func nextColumn():
+	if isEOC:
+		set_caret_column(get_caret_column() + 1)
+		return
+	var ln := get_line(get_caret_line())
+	var pos := ln.find(" ", get_caret_column())
 	set_caret_column(pos)
 	
 
-func moveLeft():
+func prevColumn():
 	var ln := get_caret_line()
 	var lineText := get_line(ln)
 	var pos := get_caret_column()
